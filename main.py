6c129aabb0599b0ec279cfcd3b93f0b73328026f
Kzzzzz111 <kevinmeng2006@outlook.com>
2025-07-25 20:01:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ from cmu_graphics import *
 
 def onAppStart(app):
     app.selectedChapter = None
-    app.Background('lightblue')
-    app.setSize(400, 400)
-    app.setTitle('PvZ Special Edition')
+    app.width = 1280
+    app.height = 720
 
 ##############################
 # start screen
 ##############################
 def start_redrawAll(app):
-    drawLabel('PvZ special edition', 100, 100, size=30, fill='black')
+    titleX = app.width / 2
+    titleY = app.height / 2 - 50
+
+    drawLabel('PvZ special edition', titleX, titleY, size=30, fill='black', bold=True)
+    drawLabel('2025 Term Project', titleX, titleY - 30, size=20, fill='black', opacity=70, bold=True)
+    drawLabel('by Kevin Meng', titleX, titleY + 30, size=10, fill='black')
 
 def start_onKeyPress(app, key):
     if key == 'space':
@@ -24,7 +28,7 @@ def chapters_onScreenActivate(app):
     app.selectedChapter = None
 
 def chapters_redrawAll(app):
-    drawLabel('Choose a chapter:', 100, 100, size=30, fill='black')
+    drawLabel('Choose a chapter with your keyboard', app.width/2, 80, size=30, fill='black')
     drawLabel('1. Chapter One', 100, 150, size=20, fill='black')
     drawLabel('2. Chapter Two', 100, 200, size=20, fill='black')
     drawLabel('3. Chapter Three', 100, 250, size=20, fill='black')
